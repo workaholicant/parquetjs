d3ca1c05fe64abcab75c5c5145314399c79739cc
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2018-02-27 21:25:37 -0500

    Add failing test

--- test/parquet-mr.js
+++ test/parquet-mr.js
@@ -43,4 +43,72 @@ describe('Parquet-mr', function() {
     const result = await readParquetMr('test-mr.parquet');
     assert.equal(result,'row group 0 \n--------------------------------------------------------------------------------\nname:      BINARY UNCOMPRESSED DO:0 FPO:4 SZ:51/51/1.00 VC:3 ENC:PLAIN,RLE\nquantity:  INT64 UNCOMPRESSED DO:0 FPO:79 SZ:46/46/1.00 VC:3 ENC:PLAIN,RLE\nprice:     DOUBLE UNCOMPRESSED DO:0 FPO:154 SZ:46/46/1.00 VC:3 ENC:PLAIN,RLE\n\n    name TV=3 RL=0 DL=0\n    ----------------------------------------------------------------------------\n    page 0:  DLE:RLE RLE:RLE VLE:PLAIN ST:[no stats for this column] SZ:29 VC:3\n\n    quantity TV=3 RL=0 DL=0\n    ----------------------------------------------------------------------------\n    page 0:  DLE:RLE RLE:RLE VLE:PLAIN ST:[no stats for this column] SZ:24 VC:3\n\n    price TV=3 RL=0 DL=0\n    ----------------------------------------------------------------------------\n    page 0:  DLE:RLE RLE:RLE VLE:PLAIN ST:[no stats for this column] SZ:24 VC:3\n\nBINARY name \n--------------------------------------------------------------------------------\n*** row group 1 of 1, values 1 to 3 *** \nvalue 1: R:0 D:0 V:apples\nvalue 2: R:0 D:0 V:oranges\nvalue 3: R:0 D:0 V:kiwi\n\nINT64 quantity \n--------------------------------------------------------------------------------\n*** row group 1 of 1, values 1 to 3 *** \nvalue 1: R:0 D:0 V:10\nvalue 2: R:0 D:0 V:20\nvalue 3: R:0 D:0 V:4\n\nDOUBLE price \n--------------------------------------------------------------------------------\n*** row group 1 of 1, values 1 to 3 *** \nvalue 1: R:0 D:0 V:2.6\nvalue 2: R:0 D:0 V:2.7\nvalue 3: R:0 D:0 V:4.2\n');
   });
+
+  it('should read a nested field', async function() {
+    var schema = new parquet.ParquetSchema({
+      fruit: {
+        fields: {
+          name: { type: 'UTF8'},
+          quantity: { type: 'INT32'}
+        }
+      }
+    });
+
+    let writer = await parquet.ParquetWriter.openFile(schema, 'test2-mr.parquet');
+
+    await writer.appendRow({
+      fruit: {
+        name: 'apple',
+        quantity: 9
+      }
+    });
+
+    await writer.close();
+
+    const result = await readParquetMr('test2-mr.parquet');
+  });
+
+  it('should read a parquetjs file with optional value', async function() {
+    var schema = new parquet.ParquetSchema({
+      name: { type: 'UTF8', optional: true }
+    });
+
+    const rows = [
+      { name: 'apples' },
+      { name: 'oranges' },
+      { name: 'kiwi' },
+    ];
+
+    let writer = await parquet.ParquetWriter.openFile(schema, 'test3-mr.parquet');
+     for (let row of rows) {
+      await writer.appendRow(row);
+    }
+
+    await writer.close();
+
+    const result = await readParquetMr('test3-mr.parquet');
+  });
+
+  it('should read repeated fields', async function() {
+    const schema =  new parquet.ParquetSchema({
+      stock: {
+        repeated: true,
+        fields: {    
+          warehouse: { type: 'UTF8' },
+        }
+      }
+    });
+
+    let writer = await parquet.ParquetWriter.openFile(schema, 'test4-mr.parquet');
+
+    await writer.appendRow({
+      stock: [
+        {warehouse: 'Newark'}
+      ]
+    });
+
+    await writer.close();
+    
+    const result = await readParquetMr('test4-mr.parquet');
+  });
 });
